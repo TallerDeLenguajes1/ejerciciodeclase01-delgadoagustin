e54dc0be6c4cabb873c3a1160f7e7d3f8929da64
Agustin Delgado <delgadoagustin147@gmail.com>
2021-08-18 18:14:13 -0300

    Agrego proyecto Adivina_numero

---
+++ Adivinar_numero/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Adivinar_numero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int numero = rnd.Next(0, 100);
+            System.Console.WriteLine("Ingresa un número");
+            int numero_ingresado;
+            numero_ingresado = Console.Read();
+            if (numero==numero_ingresado)
+            {
+                System.Console.WriteLine("Correcto");
+            }
+            else
+            {
+                System.Console.WriteLine("Equivocado, el numero era {0}",numero);
+            }
+
+        }
+    }
+}
